521f8d91e7d1f14d079ae4a2adb118a103a284fd
Megha013 <meghaawasthi013@gmail.com>
2024-11-26 21:05:27 +0530

    Add project files.

---
+++ Multithreading/Demo.cs
@@ -0,0 +1,46 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
+
+//namespace Multithreading
+//{
+//    public class Demo
+//    {
+//        public void M1()
+//        {
+//            for(int i=1; i<=5; i++)
+//            {
+//                Console.WriteLine(i);
+//            }
+//        }
+//        public void M2()
+//        {
+//            for (int i = 6; i <= 10; i++)
+//            {
+//                Console.WriteLine(i);
+//            }
+//        }
+//    }
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Demo d = new Demo();
+//            d.M1();
+//            d.M2();
+//            Thread t1 = new Thread(new ThreadStart(d.M1));
+//            //ThreadStart is a delegete
+//            //it will hold method ref that need to exec by the thread
+//            Thread t2 = new Thread(new ThreadStart(d.M1));
+//            t1.Priority = ThreadPriority.BelowNormal;
+//            t2.Priority = ThreadPriority.Highest;
+//            t1.Start();
+//            t2.Start();
+//            t1.Join(); // next all thread will be in wait state to complete the task of t1 thread
+//            t2.Join();
+//        }
+//    } 
+//}
---
+++ Multithreading/Demo2.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
+
+//namespace Multithreading
+//{
+//    public class Demo2
+//    {
+//        public void M1()
+//        {
+//            lock (this)
+//            {
+//                for(int i=1; i<=5; i++)
+//                {
+//                    Console.WriteLine(Thread.CurrentThread.Name);
+//                    Console.WriteLine(i);
+//                }
+//            }
+//        }
+//    }
+//    public class Program
+//    {   
+//        static void Main(string[] args)
+//        {
+//            Demo2 d = new Demo2();
+//            Thread t1 = new Thread(new ThreadStart(d.M1));
+//            Thread t2 = new Thread(new ThreadStart(d.M1));
+//            t1.Name = "T1";
+//            t2.Name = "T2";
+//            t1.Start();
+//            t2.Start();
+//        }
+//    }
+//}
